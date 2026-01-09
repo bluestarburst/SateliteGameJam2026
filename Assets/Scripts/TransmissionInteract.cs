d9d5bc6ddb066b0638c7bf3e290aed8c67057eea
Alfred Hale <texas.amhale@gmail.com>
2026-01-09 10:15:12 -0600

    Headphone model and cable

--- Assets/Scripts/TransmissionInteract.cs
+++ Assets/Scripts/TransmissionInteract.cs
@@ -4,11 +4,31 @@ public class TransmissionInteract : MonoBehaviour, IInteractable
 {
     public bool answeringTransmission = false;
     public float tetherDistance;
+    public GameObject cable;
+    public GameObject transmissionHeadphones;
+    public GameObject playerHeadphones;
+
+    void Awake() {
+        cable.SetActive(false);
+        transmissionHeadphones.SetActive(true);
+        playerHeadphones.SetActive(false);
+    }
 
     public void Interact(GroundPlayerInteractor interactor) {
         answeringTransmission = !answeringTransmission;
-        if (answeringTransmission) interactor.restrictMovementTo(transform.position, tetherDistance);
-        else interactor.releaseMovement();
+        VoiceSessionManager.Instance.SetLocalPlayerAtConsole(answeringTransmission);
+        if (answeringTransmission) {
+            interactor.restrictMovementTo(transform.position, tetherDistance);
+            cable.SetActive(true);
+            transmissionHeadphones.SetActive(false);
+            playerHeadphones.SetActive(true);
+        }
+        else {
+            interactor.releaseMovement();
+            cable.SetActive(false);
+            transmissionHeadphones.SetActive(true);
+            playerHeadphones.SetActive(false);
+        }
     }
 
     public void OnScroll(GroundPlayerInteractor interactor, float vertical) {
